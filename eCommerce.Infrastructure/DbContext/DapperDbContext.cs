073f6f73543cf6f5946950dbe840ee1ccc36f283
jitendra kumar <jitendra238@gmail.com>
2025-04-26 14:22:36 +0530

    environment variable added

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -11,7 +11,11 @@ namespace eCommerce.Infrastructure.DbContext
         public DapperDbContext(IConfiguration configuration)
         {
             this._configuration = configuration;
-            string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+            string? connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+            string connectionString = connectionStringTemplate
+                                       .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+                                       .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
+
 
             //Create a new npgsql connection with the retrived connection string 
             _connection =  new NpgsqlConnection(connectionString);
